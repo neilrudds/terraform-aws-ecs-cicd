8c23a408f5802b7da920044148cc19b6d74a5ed8
neilrudds <91697491+neilrudds@users.noreply.github.com>
2025-10-22 16:18:57 +0100

    formatting issue

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -46,7 +46,7 @@ resource "aws_ecs_task_definition" "nodejs-backend-task" {
         },
         {
           name  = "PORT"
-          value = var.app_port
+          value = tostring(var.app_port)
         }
       ]
       essential = true
